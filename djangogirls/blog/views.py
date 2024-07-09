7057a08dbbc9b3b9d6cdc58f8e144d5dc9f3de08
chltmdgh522 <chltmdgh517@naver.com>
2024-07-10 00:42:03 +0900

    장고 템플릿

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+
 # Create your views here.
+# 로거 생성
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all().order_by('-published_date')
+
     return render(request, 'blog/post_list.html', {'posts': posts})
